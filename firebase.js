51cc89cbdacaab9b7e85e563474df80e473e6a6b
23suit43-dotcom <23suit43@tcarts.in>
2025-09-20 23:03:34 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -6,13 +6,13 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.4/firebas
 
 // Your Firebase Config
 const firebaseConfig = {
-  apiKey: "AIzaSyAKUe5yv0-fMeyMHwXVmUVerGE8nalpJxs",
-  authDomain: "decor8-b14e8.firebaseapp.com",
-  projectId: "decor8-b14e8",
-  storageBucket: "decor8-b14e8.firebasestorage.app",
-  messagingSenderId: "301302844702",
-  appId: "1:301302844702:web:7376ed27571e8d40cccd0f",
-  measurementId: "G-NGJ1DN3SCY"
+    apiKey: "AIzaSyAKUeSyv0-fMeyMHwXVmUVerGE8nalpJxs",
+    authDomain: "decor8-b14e8.firebaseapp.com",
+    projectId: "decor8-b14e8",
+    storageBucket: "decor8-b14e8.firebasestorage.app",
+    messagingSenderId: "301302844702",
+    appId: "1:301302844702:web:7376ed27571e8d40cccd0f",
+    measurementId: "G-NGJ1DN3SCY"
 };
 
 // Initialize Firebase
@@ -21,7 +21,7 @@ const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-console.log("Firebase connected ✅");
+console.log("Firebase connected successfully!");
 
 // Export so other files can use
 export { auth, db };
