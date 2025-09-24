7b22429c6a2036a874f75222493085a124e12e6b
23suit43-dotcom <23suit43@tcarts.in>
2025-09-24 19:30:20 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -219,7 +219,7 @@
             apiKey: "AIzaSyAKUe5yv0-fMeyMHwXVmUVerGE8nalpJxs",
             authDomain: "decor8-b14e8.firebaseapp.com",
             projectId: "decor8-b14e8",
-            storageBucket: "decor8-b14e8.firebasestorage.app",
+            storageBucket: "decor8-b14e8.appspot.com"
             messagingSenderId: "301302844702",
             appId: "1:301302844702:web:7376ed27571e8d40cccd0f",
             measurementId: "G-NGJ1DN3SCY"
